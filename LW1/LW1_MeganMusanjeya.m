217a6490b0e30e5f3b06c996763341aca3eacb9d
megan-script <megan-mutsawashe.musanjeya@stud.vilniustech.lt>
2026-09-11 19:27:38 +0300

    Create LW1_MeganMusanjeya.m
    Add LW1 script

---
+++ LW1/LW1_MeganMusanjeya.m
@@ -0,0 +1,14 @@
+% Name: Megan
+% Surname: Musanjeya
+% Group: Ekfu25/1
+% Date: 2026/09/11
+
+% Paprastas skriptas
+
+x = 1:32;
+y = x.^2;
+
+plot(x, y, 'o-r', x, y/3, 'xb')
+title('Dvi funkcijos')
+xlabel('X-ai')
+ylabel('F_1 [-o-] | F_2 [-x-]')
